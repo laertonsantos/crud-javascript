0afba5d311bfb6b26636481ca5c8be6c279701bd
Gerssivaldo Oliveira Dos Santos <61440136+gerssivaldosantos@users.noreply.github.com>
2022-04-08 22:45:57 -0300

    feat: adiciona funcionalidaes dos botões de editar e deletar dados

--- js/main.js
+++ js/main.js
@@ -15,7 +15,10 @@ function getTotal(list){
 function setlist(list){
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>action</td></tr></thead><tbody> ';  
      for (var key in list){
-        table += '<tr><td>'+ formatdesc(list[key].desc) +'</td><td>'+ list[key].amount+'</td><td>'+ formatvalue(list[key].value )+'</td><td>edit | delete </td></tr><tr>';
+        table += '<tr><td>'+ 
+        formatdesc(list[key].desc) +
+        '</td><td>'+ list[key].amount+'</td><td>'+ formatvalue(list[key].value )+
+        '</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">edit</button>  <button class="btn btn-default" onclick="deleteData('+key+');">delete</button></td></tr><tr>';
      }
      table += '</tbody';
      document.getElementById("listTable").innerHTML = table
@@ -38,10 +41,57 @@ function addData(){
     var desc = document.getElementById("desc").value;
     var amount = document.getElementById("amount").value;
     var value = document.getElementById("value").value;
-
     list.unshift({"desc":desc,"amount":amount,"value":value});
     setlist(list);
 }
+
+function setUpdate (id){
+    var obj = list[id];
+    document.getElementById("desc").value = obj.desc;
+    document.getElementById("amount").value = obj.amount;
+    document.getElementById("value").value = obj.value;
+    document.getElementById("btnUpdate").style.display = "inline-block";
+    document.getElementById("btnAdd").style.display = "none";
+   
+    document.getElementById("inputIDUpdate").innerHTML = ' <input id="idupdate" type="hidden" value="'+id+'">';
+}
+
+function resetForm(){
+    document.getElementById("desc").value = "";
+    document.getElementById("amount").value = "";
+    document.getElementById("value").value = "";
+    document.getElementById("btnUpdate").style.display = "none";
+    document.getElementById("btnAdd").style.display = "inline-block";
+
+    document.getElementById("inputIDUpdate").innerHTML = "";
+}
  
+function updateData(){
+    var id = document.getElementById("idupdate").value;
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+
+    list[id] = {"desc":desc,"amount":amount,"value":value};
+    resetForm();
+    setlist(list);
+
+}
+
+      function deleteData(id){
+        if(confirm("delete this item?")){
+            if(id === list.length-1){
+                list.pop();
+            }else if(id === 0){
+                list.shift();
+            }else{
+                var arrAuxIni = list.slice(0,id);
+                var arrAuxEnd = list.slice(id+1);
+                list = arrAuxIni.concat(arrAuxEnd);
+            }   
+            setlist(list);
+        }
+    }
+
 setlist(list);
 console.log(getTotal(list));
